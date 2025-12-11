5ddbadd35a21076de869bd6102a44cfc267e45d5
giftin.id <calistanafisaa@gmail.com>
2025-12-11 15:01:00 +0700

    Update script.js

--- asset/script.js
+++ asset/script.js
@@ -15,7 +15,7 @@
 
         function checkPassword() {
             const password = document.getElementById('passwordInput').value;
-            if (password.toLowerCase() === '261025') {
+            if (password.toLowerCase() === '2711') {
                 document.getElementById('loginScreen').style.display = 'none';
                 document.getElementById('mainContent').style.display = 'block';
             } else {
